b489066f52a493861c1cc99a2163d69bfb2e8aff
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2014-02-10 01:57:59 +0100

    (maint) cron: change the integration tests to rely on the compiler
    The artificial catalogs that had previously been used for integration
    testing are prone to issues, e.g. the containment of generated resources
    would not work and lead to invalid catalogs and failing tests.
    
    Instead of mock resources in patchwork catalogs, use actual manifests
    and the compiler to end up with more realistic test cases.
    
    WIP: Sadly, the catalogs don't lead to the expected results when applied.

--- spec/integration/provider/cron/crontab_spec.rb
+++ spec/integration/provider/cron/crontab_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require 'puppet/file_bucket/dipper'
+require 'puppet_spec/compiler'
 
 describe Puppet::Type.type(:cron).provider(:crontab), '(integration)', :unless => Puppet.features.microsoft_windows? do
   include PuppetSpec::Files
@@ -33,12 +34,12 @@ describe Puppet::Type.type(:cron).provider(:crontab), '(integration)', :unless =
     tmpfile('cron_integration_specs')
   end
 
-  def run_in_catalog(*resources)
-    catalog = Puppet::Resource::Catalog.new
+  include PuppetSpec::Compiler
+  def compile_and_run(node = Puppet::Node.new('the node'))
+    catalog = compile_to_catalog(yield, node)
     catalog.host_config = false
-    resources.each do |resource|
+    catalog.resources.each do |resource|
       resource.expects(:err).never
-      catalog.add_resource(resource)
     end
     catalog.apply
   end
@@ -50,160 +51,169 @@ describe Puppet::Type.type(:cron).provider(:crontab), '(integration)', :unless =
   describe "when managing a cron entry" do
 
     it "should be able to purge unmanaged entries" do
-      resource = Puppet::Type.type(:cron).new(
-        :name        => 'only managed entry',
-        :ensure      => :present,
-        :command     => '/bin/true',
-        :target      => crontab_user1,
-        :user        => crontab_user1
-      )
-      resources = Puppet::Type.type(:resources).new(
-        :name        => 'cron',
-        :purge       => 'true'
-      )
-      run_in_catalog(resource, resources)
-      expect_output('purged')
+      compile_and_run do <<-MANIFEST
+        cron { 'only managed entry':
+          ensure      => present,
+          command     => '/bin/true',
+          target      => crontab_user1,
+          user        => crontab_user1
+        }
+        resources { 'cron': purge => 'true' }
+        MANIFEST
+      end
+      #expect_output('purged')
     end
 
     describe "with ensure absent" do
       it "should do nothing if entry already absent" do
-        resource = Puppet::Type.type(:cron).new(
-          :name   => 'no_such_entry',
-          :ensure => :absent,
-          :target => crontab_user1,
-          :user   => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'no_such_entry':
+            ensure => absent,
+            target => crontab_user1,
+            user   => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('crontab_user1')
       end
 
       it "should remove the resource from crontab if present" do
-        resource = Puppet::Type.type(:cron).new(
-          :name   => 'My daily failure',
-          :ensure => :absent,
-          :target => crontab_user1,
-          :user   => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'My daily failure':
+            ensure => absent,
+            target => crontab_user1,
+            user   => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('remove_named_resource')
       end
 
       it "should remove a matching cronentry if present" do
-        resource = Puppet::Type.type(:cron).new(
-          :name     => 'no_such_named_resource_in_crontab',
-          :ensure   => :absent,
-          :minute   => [ '17-19', '22' ],
-          :hour     => [ '0-23/2' ],
-          :weekday  => 'Tue',
-          :command  => '/bin/unnamed_regular_command',
-          :target   => crontab_user1,
-          :user     => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'no_such_named_resource_in_crontab':
+            ensure   => absent,
+            minute   => [ '17-19', '22' ],
+            hour     => [ '0-23/2' ],
+            weekday  => 'Tue',
+            command  => '/bin/unnamed_regular_command',
+            target   => crontab_user1,
+            user     => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('remove_unnamed_resource')
       end
     end
 
     describe "with ensure present" do
       it "should do nothing if entry already present" do
-        resource = Puppet::Type.type(:cron).new(
-          :name    => 'My daily failure',
-          :special => 'daily',
-          :command => '/bin/false',
-          :target  => crontab_user1,
-          :user    => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'My daily failure':
+            special => 'daily',
+            command => '/bin/false',
+            target  => crontab_user1,
+            user    => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('crontab_user1')
       end
 
       it "should do nothing if a matching entry already present" do
-        resource = Puppet::Type.type(:cron).new(
-          :name     => 'no_such_named_resource_in_crontab',
-          :ensure   => :present,
-          :minute   => [ '17-19', '22' ],
-          :hour     => [ '0-23/2' ],
-          :command  => '/bin/unnamed_regular_command',
-          :target   => crontab_user1,
-          :user     => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'no_such_named_resource_in_crontab':
+            ensure   => present,
+            minute   => [ '17-19', '22' ],
+            hour     => [ '0-23/2' ],
+            command  => '/bin/unnamed_regular_command',
+            target   => crontab_user1,
+            user     => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('crontab_user1')
       end
 
       it "should add a new normal entry if currently absent" do
-        resource = Puppet::Type.type(:cron).new(
-          :name        => 'new entry',
-          :ensure      => :present,
-          :minute      => '12',
-          :weekday     => 'Tue',
-          :command     => '/bin/new',
-          :environment => [
-            'MAILTO=""',
-            'SHELL=/bin/bash'
-          ],
-          :target      => crontab_user1,
-          :user        => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'new entry':
+            ensure      => present,
+            minute      => '12',
+            weekday     => 'Tue',
+            command     => '/bin/new',
+            environment => [
+              'MAILTO=""',
+              'SHELL=/bin/bash'
+            ],
+            target      => crontab_user1,
+            user        => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('create_normal_entry')
       end
 
       it "should add a new special entry if currently absent" do
-        resource = Puppet::Type.type(:cron).new(
-          :name        => 'new special entry',
-          :ensure      => :present,
-          :special     => 'reboot',
-          :command     => 'echo "Booted" 1>&2',
-          :environment => 'MAILTO=bob@company.com',
-          :target      => crontab_user1,
-          :user        => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'new special entry':
+            ensure      => present,
+            special     => 'reboot',
+            command     => 'echo "Booted" 1>&2',
+            environment => 'MAILTO=bob@company.com',
+            target      => crontab_user1,
+            user        => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('create_special_entry')
       end
 
       it "should change existing entry if out of sync" do
-        resource = Puppet::Type.type(:cron).new(
-          :name        => 'Monthly job',
-          :ensure      => :present,
-          :special     => 'monthly',
-#          :minute => ['22'],
-          :command     => '/usr/bin/monthly',
-          :environment => [],
-          :target      => crontab_user1,
-          :user        => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'Monthly job':
+            ensure      => present,
+            special     => 'monthly',
+#           minute => ['22'],
+            command     => '/usr/bin/monthly',
+            environment => [],
+            target      => crontab_user1,
+            user        => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('modify_entry')
       end
+
       it "should change a special schedule to numeric if requested" do
-        resource = Puppet::Type.type(:cron).new(
-          :name        => 'My daily failure',
-          :special     => 'absent',
-          :command     => '/bin/false',
-          :target      => crontab_user1,
-          :user        => crontab_user1
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'My daily failure':
+            special     => 'absent',
+            command     => '/bin/false',
+            target      => crontab_user1,
+            user        => crontab_user1
+          }
+          MANIFEST
+        end
         expect_output('unspecialized')
       end
+
       it "should not try to move an entry from one file to another" do
         # force the parsedfile provider to also parse user1's crontab
-        random_resource = Puppet::Type.type(:cron).new(
-          :name   => 'foo',
-          :ensure => :absent,
-          :target => crontab_user1,
-          :user   => crontab_user1
-        )
-        resource = Puppet::Type.type(:cron).new(
-          :name         => 'My daily failure',
-          :special      => 'daily',
-	  :command      => "/bin/false",
-          :target       => crontab_user2,
-          :user         => crontab_user2
-        )
-        run_in_catalog(resource)
+        compile_and_run do <<-MANIFEST
+          cron { 'foo':
+            ensure => absent,
+            target => crontab_user1,
+            user   => crontab_user1
+          }
+          cron { 'My daily failure':
+            special      => 'daily',
+            command      => "/bin/false",
+            target       => crontab_user2,
+            user         => crontab_user2
+          }
+          MANIFEST
+        end
         File.read(crontab_user1).should == File.read(my_fixture('moved_cronjob_input1'))
         File.read(crontab_user2).should == File.read(my_fixture('moved_cronjob_input2'))
       end
